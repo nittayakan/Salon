468fae6ca274e735610352664ff02fec22ba13a9
nittayakan <nittayakan.pond@hotmail.com>
2018-11-26 18:26:07 +0700

    แก้
    แก้ title

--- promotion.php
+++ promotion.php
@@ -2,7 +2,7 @@
 <html>
 <head> 
 <meta name="viewport" content="width=device-width, initial-scale=1">      
-<title>Promotion2222222222</title>
+<title>Promotion</title>
 
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
